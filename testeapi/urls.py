aa318ab1918c14a1861a222877d5a6029055fcb4
tilherme <gui.mauricio@hotmail.com.br>
2022-02-09 00:10:45 -0300

    fix: corrigindo login

--- testeapi/urls.py
+++ testeapi/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+# from django.conf.urls.static import static
+from django.conf.urls import url, include
+from core import urls as core_urls
 
 urlpatterns = [
+    url(r'^core/', include(core_urls)),
     path('admin/', admin.site.urls),
 ]
